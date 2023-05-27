dbf2a28d53b4e19d442981bffb9476da7f64cb77
stefaN-N-N <stefanlepsanovic@gmail.com>
2023-05-27 12:15:20 +0200

    End of section 5

--- client/src/features/about/AboutPage.tsx
+++ client/src/features/about/AboutPage.tsx
@@ -1,5 +1,79 @@
-import { Typography } from "@mui/material"
+import {
+  Alert,
+  AlertTitle,
+  Button,
+  ButtonGroup,
+  Container,
+  List,
+  ListItem,
+  ListItemText,
+  Typography,
+} from "@mui/material"
+import agent from "../../app/api/agent"
+import { useState } from "react"
 
 export default function AboutPage() {
-  return <Typography variant="h2">About page</Typography>
+  const [validationErrors, setValidationErrors] = useState<string[]>([])
+
+  function getValidatdionError() {
+    agent.TestErrors.getValidationError()
+      .then(() => console.log("shouldnt see this"))
+      .catch((error) => setValidationErrors(error))
+  }
+  return (
+    <Container>
+      <Typography gutterBottom variant="h2">
+        Errors for testing
+      </Typography>
+      <ButtonGroup fullWidth>
+        <Button
+          variant="contained"
+          onClick={() =>
+            agent.TestErrors.get400Error().catch((error) => console.log(error))
+          }
+        >
+          Test 400 error
+        </Button>
+        <Button
+          variant="contained"
+          onClick={() =>
+            agent.TestErrors.get401Error().catch((error) => console.log(error))
+          }
+        >
+          Test 401 error
+        </Button>
+        <Button
+          variant="contained"
+          onClick={() =>
+            agent.TestErrors.get404Error().catch((error) => console.log(error))
+          }
+        >
+          Test 404 error
+        </Button>
+        <Button
+          variant="contained"
+          onClick={() =>
+            agent.TestErrors.get500Error().catch((error) => console.log(error))
+          }
+        >
+          Test 500 error
+        </Button>
+        <Button variant="contained" onClick={getValidatdionError}>
+          Validation error
+        </Button>
+      </ButtonGroup>
+      {validationErrors.length > 0 && (
+        <Alert severity="error">
+          <AlertTitle>ValidationErrors</AlertTitle>
+          <List>
+            {validationErrors.map((error) => (
+              <ListItem key={error}>
+                <ListItemText>{error}</ListItemText>
+              </ListItem>
+            ))}
+          </List>
+        </Alert>
+      )}
+    </Container>
+  )
 }
--- client/src/features/catalog/ProductDetails.tsx
+++ client/src/features/catalog/ProductDetails.tsx
@@ -9,9 +9,12 @@ import {
   Typography,
 } from "@mui/material"
 import axios from "axios"
-import { useEffect, useState } from "react"
+import { useDebugValue, useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 import { Product } from "../../app/models/product"
+import agent from "../../app/api/agent"
+import NotFound from "../../app/errors/NotFound"
+import LoadingComponent from "../../app/layout/LoadingComponent"
 
 export default function ProductDetails() {
   const { id } = useParams<{ id: string }>()
@@ -19,16 +22,24 @@ export default function ProductDetails() {
   const [loading, setLoading] = useState(true)
 
   useEffect(() => {
-    axios
-      .get(`http://localhost:5145/api/products/${id}`)
-      .then((response) => setProduct(response.data))
-      .catch((error) => console.log(error))
-      .finally(() => setLoading(false))
+    id &&
+      agent.Catalog.details(parseInt(id))
+        .then((response) => setProduct(response))
+        .catch((error) => console.log(error))
+        .finally(() => setLoading(false))
   }, [id])
 
-  if (loading) return <h3>Loading...</h3>
+  // useEffect(() => {
+  //   axios
+  //     .get(`http://localhost:5145/api/products/${id}`)
+  //     .then((response) => setProduct(response.data))
+  //     .catch((error) => console.log(error))
+  //     .finally(() => setLoading(false))
+  // }, [id])
 
-  if (!product) return <h3>Product not found</h3>
+  if (loading) return <LoadingComponent message="Loading product..." />
+
+  if (!product) return <NotFound />
 
   return (
     <Grid container spacing={6}>
